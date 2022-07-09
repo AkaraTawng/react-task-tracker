aea18f4069231abc37278f12a43ba63a61189ab9
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-09 07:09:24 +0700

    add color and text props to Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <header className='header'> 
             <h1>Task Tracker</h1>
-            <Button />
+            <Button color='green' text='hello'/>
         </header>
     )
 }
