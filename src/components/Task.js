7b719bc70c28a0bc0e924fc5c0b443a02cc8edae
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-11 05:53:17 +0700

    import FA icon. add delete icon to task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
+import {FaTimes} from 'react-icons/fa';
 import React from 'react';
 
 const Task = ({task}) => {
     return (
         <div className='task' >
-            <h3>{task.text}</h3>
+            <h3>{task.text} <FaTimes/></h3>
             <p>{task.day}</p>
         </div>
     )
