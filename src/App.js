81de240946e8b66bad795a6896d869cc4f02ed0e
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-07-11 08:25:47 +0700

    add showAddTask state. conditionally render form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import {useState} from 'react';
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -43,7 +44,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
-      <AddTask onAdd={addTask}/>
+      { showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
